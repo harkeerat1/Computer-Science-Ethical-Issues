6d8ae7df4bbffc903a3dd48b370b15737f44afef
harkeerat1 <134604505+harkeerat1@users.noreply.github.com>
2023-05-31 10:38:59 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) =>{
+        console.log(entry)
+
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        } else{
+            entry.target.classList.remove('show');
+        }
+
+        if(entry.isIntersecting){
+            entry.target.classList.add('show1');
+        } else{
+            entry.target.classList.remove('show1');
+        }
+    });
+
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+hiddenElements.forEach((el) => observer.observe(el));
